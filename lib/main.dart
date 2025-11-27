48c72445e83a37781f3b80ee4c644e71bccaa1ba
ikacahya247 <ikazzip3@gmail.com>
2025-11-27 21:34:48 +0700

    selesaisuperbase

---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+// lib/main.dart
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'utils/app_routes.dart';
+import 'utils/constants.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  
+  // Inisialisasi GetStorage
+  await GetStorage.init();
+
+  // Inisialisasi Supabase
+  await Supabase.initialize(
+    url: SupabaseCredentials.supabaseUrl,
+    anonKey: SupabaseCredentials.supabaseAnonKey,
+  );
+
+  runApp(const MyApp());
+}
+
+// Helper global untuk akses mudah ke client Supabase
+final supabase = Supabase.instance.client;
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Supabase',
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+      ),
+      initialRoute: AppRoutes.splash,
+      getPages: AppRoutes.routes,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+// lib/utils/constants.dart
+
+class SupabaseCredentials {
+  static const String supabaseUrl = 'https://dnsziwclptjpmrwiefrv.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuc3ppd2NscHRqcG1yd2llZnJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NDAyNTgsImV4cCI6MjA3OTAxNjI1OH0.XNwYMekJV0GZIB_1NurwiHDzgc4cL1s5UfFbHqdZPQ8';
+}
